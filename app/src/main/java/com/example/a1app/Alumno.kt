bdeae72af774180768e2dc1952af3d8e1c41a142
Javier <javmar421@alu.centroafuera.es>
2021-09-15 11:09:07 +0200

    Idiomas

---
+++ app/src/main/java/com/example/a1app/Alumno.kt
@@ -0,0 +1,4 @@
+package com.example.a1app
+
+class Alumno {
+}
--- app/src/main/java/com/example/a1app/MainActivity.kt
+++ app/src/main/java/com/example/a1app/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.a1app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Log Import Required
+        Log.v("MainActivity", "Se ha creado correctamente")
+        //Type de LOG
+        // v valid d debug i important
+        // w warning e error
+        Toast.makeText(this, getString(R.string.hello), Toast.LENGTH_LONG).show()
 
     }
 }
